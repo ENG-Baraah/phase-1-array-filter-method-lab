9bb9037efb400c7c728fe0e410757eac96e1ef6b
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-04-02 15:56:18 +0300

    Completed Using the Array Filter Method Assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+
+// const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, s) {
+  const arr = drivers.filter((ele) => ele.toLowerCase() === s.toLowerCase());
+  return arr;
+}
+
+function fuzzyMatch(drivers, s) {
+  const arr = drivers.filter((ele) =>
+    ele.toLowerCase().startsWith(s.toLowerCase())
+  );
+  return arr;
+}
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(drivers, s) {
+  const arr = drivers.filter(
+    (ele) => ele.name.toLowerCase() === s.toLowerCase()
+  );
+  return arr;
+}
